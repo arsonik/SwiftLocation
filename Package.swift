d4012428bb1d1bfafe8062341bf0a4282ab4d686
Florian Morello <arsonik@me.com>
2019-06-05 07:45:05 +0200

    remove path

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftLocation",
-            path: "Sources",
             targets: ["SwiftLocation"]),
     ],
     dependencies: [
