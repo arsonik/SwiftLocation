7710199a63839cc1ba431c9afa5647f35ef93395
Florian Morello <arsonik@me.com>
2019-06-05 07:43:57 +0200

    move path

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLocation",
-    path: "Sources",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftLocation",
+            path: "Sources",
             targets: ["SwiftLocation"]),
     ],
     dependencies: [
